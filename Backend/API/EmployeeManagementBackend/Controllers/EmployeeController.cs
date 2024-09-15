7f702a5fbf8681f534fc996816ba2c72d63d64c8
Pulok Ahmed <pulokahmed07@gmail.com>
2024-09-15 15:21:35 +0600

    backend api project added with db context file

---
+++ Backend/API/EmployeeManagementBackend/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagementBackend.Data;
+using EmployeeManagementBackend.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementBackend.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public EmployeeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        [Route("api/employee/getallemployee")]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAllEmployee()
+        {
+            return await _db.Employees.ToListAsync();
+        }
+
+    }
+}
