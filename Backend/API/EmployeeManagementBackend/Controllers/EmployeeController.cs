1958f86782de08406e801d4f8868c2c0050e0c0b
Pulok Ahmed <pulokahmed07@gmail.com>
2024-09-23 16:37:18 +0600

    frontend api:Add employee functionality added

--- Backend/API/EmployeeManagementBackend/Controllers/EmployeeController.cs
+++ Backend/API/EmployeeManagementBackend/Controllers/EmployeeController.cs
@@ -21,8 +21,13 @@ namespace EmployeeManagementBackend.Controllers
 
         [HttpPost]
         [Route("api/employee/addnewemployee")]
-        public async Task<ActionResult<Employee>> AddNewEmployee(Employee employee)
+        public async Task<ActionResult<Employee>> AddNewEmployee([FromBody] Employee employee)
         {
+
+            if (employee == null || string.IsNullOrEmpty(employee.Name) || string.IsNullOrEmpty(employee.Email) || string.IsNullOrEmpty(employee.Phone))
+            {
+                return BadRequest("Invalid employee data.");
+            }
             _db.Employees.Add(employee);
             await _db.SaveChangesAsync();
             return CreatedAtAction("GetAllEmployee", new { id = employee.Id }, employee);
--- FrontEnd/EmployeeManagementFrontEnd/src/app/app.component.ts
+++ FrontEnd/EmployeeManagementFrontEnd/src/app/app.component.ts
@@ -4,23 +4,57 @@ import { RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Employee } from '../models/employee.model';
 import { AsyncPipe } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HttpClientModule,AsyncPipe],
+  imports: [RouterOutlet,HttpClientModule,AsyncPipe,FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'EmployeeManagementFrontEnd';
   http=inject(HttpClient);
+  
+  contactsForm = new FormGroup({ 
+
+    name: new FormControl<string>(''),
+    email: new FormControl<string | null>(null), 
+    phone: new FormControl<string | null>(null),
+  })
+
+  onFormSubmit() {
+    
+
+  const addContactRequest={
+    name: this.contactsForm.value.name,
+    email: this.contactsForm.value.email,
+    phone: this.contactsForm.value.phone,
+  }
+  console.log(addContactRequest);
+
+  this.http.post('https://localhost:7085/api/employee/addnewemployee', addContactRequest)
+    .subscribe({
+        next: (value) => {
+          console.log(value);
+          this.employees$=this.getAllEmployee();
+          this.contactsForm.reset();
+        }
+    });
+    console.log(this.contactsForm.value);
+  }
+
   employees$=this.getAllEmployee();
 
   private getAllEmployee():Observable<Employee[]>{
 
     return this.http.get<Employee[]>("https://localhost:7085/api/employee/getallemployee");
+  }
+  private addEmployee():Observable<Employee[]>{
+
+    return this.http.get<Employee[]>("https://localhost:7085/api/employee/addnewemployee");
   }
 }
